361ad9ed855bde28cf70a52d91783030910318e6
Egor Loshkarev <Johnnylao@mail.ru>
2023-08-30 01:36:28 +0600

    v 0.3

--- static/js/swiper.js
+++ static/js/swiper.js
@@ -1,9 +1,19 @@
 var swiper = new Swiper(".mySwiper", {
-      spaceBetween: 30,
-      effect: "fade",
-      centeredSlides: true,
+      grabCursor: true,
+      effect: "coverflow",
+      speed: 800,
+      loop: true,
       autoplay: {
-        delay: 5000,
+        delay: 4000,
         disableOnInteraction: false,
+      },
+      creativeEffect: {
+        prev: {
+          shadow: true,
+          translate: [0, 0, -400],
+        },
+        next: {
+          translate: ["100%", 0, 0],
+        },
       },
     });
